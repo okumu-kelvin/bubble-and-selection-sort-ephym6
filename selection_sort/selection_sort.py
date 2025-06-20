c5c8b190651e75b7ffe1f8bb3bf964699958e507
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-06-20 14:06:51 +0300

    Selection Sort Algorithm done

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,23 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+
+    n = len(arr)
+    for i in range(n):
+        # Minimum element
+        min_element = i
+        # Find index of minimum element in unsorted array
+        for j in range(i+1, n):
+            if arr[min_element] > arr[j]:
+                min_element = j
+
+        # Implement selection sort by swapping the new minimum element with first element of unsorted array
+        arr[i], arr[min_element] = arr[min_element], arr[i]
+
+    return arr
+    # pass
+
+#Example
+sample_list = [10, 5, 2, 3, 1, 100]
+print("Unsorted list: ", sample_list)
+#Implement selection sort
+print("Sorted list: ", selection_sort(sample_list))
