af52201877c05fda8a05de3e8051307ccfc164f0
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-06-20 13:51:51 +0300

    Bubble Sort Algorithm tested

--- bubble_sort/test_bubble_sort.py
+++ bubble_sort/test_bubble_sort.py
@@ -2,9 +2,20 @@ from bubble_sort import bubble_sort
 
 def test_sorted():
     assert bubble_sort([1, 2, 3]) == [1, 2, 3]
+    # return array to be sorted using bubble_sort
+    return [1, 2, 3]
 
 def test_reverse():
     assert bubble_sort([3, 2, 1]) == [1, 2, 3]
+    # return array to be sorted using bubble_sort
+    return [3, 2, 1]
 
 def test_duplicates():
     assert bubble_sort([4, 5, 3, 4]) == [3, 4, 4, 5]
+    # return array to be sorted using bubble_sort
+    return [4, 5, 3, 4]
+
+# Implement tests
+print("Sorted (using test_sorted()): ", bubble_sort(test_reverse()))
+print("Sorted (using test_reverse()): ", bubble_sort(test_reverse()))
+print("Sorted (using test_sorted()): ", bubble_sort(test_duplicates()))
